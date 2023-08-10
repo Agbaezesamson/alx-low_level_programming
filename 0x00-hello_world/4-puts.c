65ee29f6c9fefbd9d55f00024523ba3ad09f276c
Agbaezesamson <agbaezesamson333@gmail.com>
2023-08-10 16:39:37 -0700

    use the put functions in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <studio.h>
-
+/**
+* main - A program that print a line with puts function
+* Return: 0 (Success)
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle,");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
