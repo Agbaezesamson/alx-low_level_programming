3ae8208eed701bf9ec679c5751974e0364eae51c
Agbaezesamson <agbaezesamson333@gmail.com>
2023-08-28 09:33:08 -0700

    Write a function that locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - Entry point of data
+ * @s: input data
+ * @c: input data
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
