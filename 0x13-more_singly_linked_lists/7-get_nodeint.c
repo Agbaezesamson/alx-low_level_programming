4e204cabe719871da4fb12209fca3cce826240df
Agbaeze Samson <Agbaezesamson333@gmail.com>
2023-09-26 12:46:26 -0700

    retrieves a node at a specified index in a singly linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- * get_nodeint_at_index - returns the node at a specified index in a linked list
+ * get_nodeint_at_index - returns the node to a index in a linked list
  * @head: pointer to the first node of the linked list
  * @index: index of the node to retrieve
  *
- * Return: pointer to the node at the specified index, or NULL if not found
+ * Return: pointer to the node at the index, or NULL if not found
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
