05ef519dc38f5526ec8e97eabffcf5fd9d4956e8
Agbaeze Samson <Agbaezesamson333@gmail.com>
2023-09-26 14:12:16 -0700

    Free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,8 +2,11 @@
 #include <stdio.h>
 
 /**
- * Function free_listint: Releases the memory occupied by a linked list
- * @head: Pointer to the beginning of the listint_t list to be deallocated
+ * free_listint - Frees a listint_t linked list.
+ * @head: A pointer to the head of the list.
+ *
+ * Description: This function frees the memory occupied by a linked list
+ * of type listint_t.
  */
 void free_listint(listint_t *head)
 {
